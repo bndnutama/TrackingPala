9fc480dd4fdae06dae40b066db1ea9022de4844c
bndnutama <136488465+bndnutama@users.noreply.github.com>
2025-11-19 22:28:04 +0700

    settings.py

--- Palachain/settings.py
+++ Palachain/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
 ROOT_URLCONF = 'Palachain.urls'
 AUTH_USER_MODEL = 'tracking.ProfileUser'
@@ -115,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static",]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
